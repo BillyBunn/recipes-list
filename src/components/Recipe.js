56fe60fb5f522ef55326b175dc56af37d39dea79
Billy Bunn <40321457+BillyBunn@users.noreply.github.com>
2019-10-01 19:53:53 -0700

    adds style and temp select

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-
-const TEMP_UNITS = "fa";
+import styled from "styled-components";
+import moment from "moment";
 
 const Recipe = ({ recipe }) => {
   const { title, description, steps } = recipe;
@@ -15,25 +15,46 @@ const Recipe = ({ recipe }) => {
     if (!firstStep) setCurrentStep(currentStep - 1);
   };
 
+  const [tempUnits, setTempUnits] = useState("fa");
+  const handleTempSelect = e => {
+    setTempUnits(e.target.value);
+  };
+
   return (
-    <div>
-      <h4>Recipe: {title}</h4>
+    <StyledRecipe>
+      <h2>Recipe: {title}</h2>
       <p>{description}</p>
       <div>
-        <Step step={steps[currentStep]} num={currentStep + 1} />
-        <button onClick={handlePrev}>Prev</button>
-        <button onClick={handleNext}>Next</button>
+        <Step
+          step={steps[currentStep]}
+          num={currentStep + 1}
+          tempUnits={tempUnits}
+        />
+        <div>
+          <button onClick={handlePrev} disabled={firstStep}>
+            Prev
+          </button>
+          <button onClick={handleNext} disabled={lastStep}>
+            Next
+          </button>
+        </div>
       </div>
-    </div>
+      <label>
+        Units
+        <select value={tempUnits} onChange={handleTempSelect}>
+          <option value="fa">Fahrenheit</option>
+          <option value="ce">Celcius</option>
+        </select>
+      </label>
+    </StyledRecipe>
   );
 };
 
-const Step = ({ step, num }) => {
-  const title = `Step ${num}. ${step.title}`;
+const Step = ({ step, num, tempUnits }) => {
   const range = step.timeTemp.reduce(
     (acc, val) => {
       let { time, temp } = val;
-      temp = temp[TEMP_UNITS];
+      temp = temp[tempUnits];
       if (time < acc.time.min) acc.time.min = time;
       if (time > acc.time.max) acc.time.max = time;
       if (temp < acc.temp.min) acc.temp.min = temp;
@@ -47,23 +68,63 @@ const Step = ({ step, num }) => {
   );
   console.log(range);
 
-  const time = `${range.time.min} – ${range.time.max}`;
+  const time = `${moment.utc(range.time.min).format("mm:ss")} – ${moment
+    .utc(range.time.max)
+    .format("mm:ss")} minutes`;
   const temp = `${range.temp.min} – ${range.temp.max}`;
 
   return (
     <>
-      <h5>{title}</h5>
-      <p>{step.description}</p>
+      <h3>
+        Step {num}:<br />
+        {step.title}
+      </h3>
+      <p className="recipe-description">{step.description}</p>
       <select>
         <option value="">Desired doneness</option>
         {step.timeTemp.map(({ doneness }) => (
           <option value={doneness}>{doneness}</option>
         ))}
       </select>
-      <p>Temp: {temp}</p>
-      <p>Time: {time}</p>
+      <p>
+        Temperature (&deg;{tempUnits === "fa" ? "F" : "C"}):
+        <br />
+        {temp}
+      </p>
+      <p>
+        Time:
+        <br />
+        {time}
+      </p>
     </>
   );
 };
+
+const StyledRecipe = styled.div`
+  display: flex;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica,
+    Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  flex-flow: column nowrap;
+  margin: 0 auto;
+  max-width: 30em;
+  > div {
+    align-items: center;
+    border: 2px solid black;
+    border-radius: 8px;
+    display: flex;
+    flex-flow: column nowrap;
+    padding: 1em;
+    > h3 {
+      margin: 0;
+      text-align: center;
+    }
+    > p {
+      text-align: center;
+    }
+  }
+  > label {
+    margin: 1em auto;
+  }
+`;
 
 export default Recipe;
