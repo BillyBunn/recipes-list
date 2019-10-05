cd689bf730cf716db2d2ece187510fa689c37a65
Billy Bunn <40321457+BillyBunn@users.noreply.github.com>
2019-10-05 13:39:23 -0700

    starts timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,25 +1,30 @@
 import React, { useState } from "react";
 
 const Timer = ({ totalTime }) => {
-  const [time, setTime] = useState(totalTime);
+  const [time, setTime] = useState(100);
+  const [seconds, setSeconds] = useState(100);
   const [ticking, setTicking] = useState(false);
-
+  const timer = 0;
   const startTimer = () => {
-    if (!ticking) setInterval(tick, 1000);
-    if (ticking) clearInterval(tick);
-    setTicking(!ticking);
+    if (timer === 0 && seconds > 0) {
+      timer = setInterval(tick, 1000);
+    }
   };
 
   const tick = () => {
-    console.log("tick", time.seconds);
-    let { hours, minutes, seconds } = time;
-    seconds -= 1;
-    setTime({ seconds });
+    // Remove one second, set state so a re-render happens.
+    setTime(seconds - 1);
+    setSeconds(seconds - 1);
+
+    // Check if we're at zero.
+    if (seconds === 0) {
+      clearInterval(timer);
+    }
   };
 
   return (
     <div>
-      <span>{time.seconds}</span>
+      <span>{time}</span>
       <button onClick={startTimer}>{ticking ? "Stop" : "Start"}</button>
     </div>
   );
